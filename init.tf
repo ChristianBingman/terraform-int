569f96cc3c6f4afe562bc9ce70ba66d956d73241
ChristianBingman <christianbingman@gmail.com>
2024-10-17 18:49:52 -0500

    Add base

---
+++ init.tf
@@ -0,0 +1,84 @@
+terraform {
+  required_providers {
+    helm = {
+      source = "hashicorp/helm"
+      version = "2.15.0"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.32.0"
+    }
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+module "metrics-server" {
+   source = "./modules/metrics_server"
+}
+
+module "cert-manager" {
+  source = "./modules/cert-manager"
+  cloudflare_api_token = var.cert-manager_cloudflare_api_token
+  servicemonitor_enabled = true
+  depends_on = [module.prometheus-stack]
+}
+
+module "longhorn" {
+  source = "./modules/longhorn"
+  smb_username = var.longhorn_smb_username
+  smb_password = var.longhorn_smb_password
+}
+
+module "nfs-subdir" {
+  source = "./modules/nfs-subdir-external-provisioner"
+}
+
+module "prometheus-stack" {
+  source = "./modules/prometheus-stack"
+  admin_password = var.prometheus-stack_grafana_admin_password
+  smtp_host = var.smtp_host
+  smtp_user = var.smtp_user
+  smtp_pass = var.smtp_pass
+  depends_on = [module.longhorn]
+}
+
+module "metallb" {
+  source = "./modules/metallb"
+}
+
+module "eck-operator" {
+  source = "./modules/eck"
+  depends_on = [module.longhorn]
+}
+
+module "github-arc" {
+  source = "./modules/github_arc"
+  github_pat = var.github_pat
+}
+
+module "elasticsearch-int" {
+  source = "./modules/elasticsearch-int"
+  depends_on = [module.eck-operator, module.nfs-subdir]
+}
+
+module "nginx-ingress" {
+  source = "./modules/nginx_ingress"
+  metallb_ip = "10.2.0.41"
+}
+
+module "argocd" {
+  source = "./modules/argocd"
+}
+
+module "grafana-dashboards" {
+  source = "./modules/grafana-dashboards"
+}
---
+++ variables.tf
@@ -0,0 +1,45 @@
+variable "prometheus-stack_grafana_admin_password" {
+  type = string
+}
+
+variable "smtp_host" {
+  type = string
+}
+variable "smtp_user" {
+  type = string
+}
+variable "smtp_pass" {
+  type = string
+}
+
+variable "cert-manager_cloudflare_api_token" {
+  type = string
+}
+
+variable "cloudflare-tunnel_credentials" {
+  type = string
+}
+
+variable "github_pat" {
+  type = string
+}
+
+variable "github_client_id" {
+  type = string
+}
+
+variable "github_client_secret" {
+  type = string
+}
+
+variable "drone_rpc_secret" {
+  type = string
+}
+
+variable "longhorn_smb_username" {
+  type = string
+}
+
+variable "longhorn_smb_password" {
+  type = string
+}
