048535e06b48b5fb2b9e59e1765102a8142ec715
ChristianBingman <christianbingman@gmail.com>
2024-10-30 18:51:41 -0500

    Add argocd

---
+++ modules/argocd/main.tf
@@ -0,0 +1,34 @@
+resource "helm_release" "argocd" {
+  name       = "argocd"
+
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  namespace  = var.namespace
+  create_namespace = true
+
+  values = [
+    <<-EOT
+    global:
+      domain: argocd.int.christianbingman.com
+
+    configs:
+      params:
+        server.insecure: true
+
+    server:
+      ingress:
+        enabled: true
+        ingressClassName: nginx
+        annotations:
+          nginx.ingress.kubernetes.io/force-ssl-redirect: "true"
+          nginx.ingress.kubernetes.io/backend-protocol: "HTTP"
+          cert-manager.io/cluster-issuer: "le-christianbingman-com"
+        extraTls:
+          - hosts:
+            - argocd.int.christianbingman.com
+            # Based on the ingress controller used secret might be optional
+            secretName: wildcard-tls
+    EOT
+  ]
+
+}
